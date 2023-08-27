55f965d9452655132d48ee98429d8e675b138591
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-08-28 00:18:29 +0100

    createprompt

--- app/create-prompt/page.js
+++ app/create-prompt/page.js
@@ -1,27 +1,50 @@
 "use client";
 
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import Form from "@components/Form";
-import React, { useState } from "react";
 
 const CreatePrompt = () => {
-  const [submitting, setsubmitting] = useState(false);
-  const [post, setpost] = useState({
-    tag: "",
-    prompt: "",
-  });
+  const router = useRouter();
+  const { data: session } = useSession();
 
-  const createPrompt = async () => {};
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setpost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
-    <div>
-      <Form
-        type="Create"
-        post={post}
-        setpost={setpost}
-        submitting={submitting}
-        handleSumbit={createPrompt}
-      />
-    </div>
+    <Form
+      type="Create"
+      post={post}
+      setpost={setpost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
   );
 };
 
